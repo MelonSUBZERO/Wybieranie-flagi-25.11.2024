4ebc01baa63542cf412d2011f4cbb59c23c7565b
MelonSUBZERO <matimagdon@gmail.com>
2024-12-01 22:52:22 +0100

    KONIEC OSTATNIA WERSJA

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let niepoprawneP = document.querySelector('#niep');
 let poprawneP = document.querySelector('#pop');
 let rekord = localStorage.getItem('rekord') ? parseInt(localStorage.getItem('rekord')) : 0;
 const rekordP = document.querySelector("#rekord");
+const body = document.querySelector('body')
+const vid = document.createElement('video')
+body.appendChild(vid)
+vid.style.visibility = 'hidden'
 let interwal = setInterval(() => {
     rekordP.style.color = `rgb(${Math.ceil(Math.random()*255)}, ${Math.ceil(Math.random()*255)}, ${Math.ceil(Math.random()*255)})`
 }, 1000);
@@ -52,23 +56,41 @@ async function lewy() {
     }
 
     if (niep == 5) {
-        alert().name = 'KONIEC GRY'
         alert(`Koniec gry, przegrałeś! Osiągnąłeś wynik ${popr} poprawnych odpowiedzi`);
         window.location.reload(true);
     }
 
     if (country1.population > country2.population) {
         popr++;
+        vid.src = 'dobrze.mp4'
+        vid.autoplay = true
+        vid.loop = false
+        const flaga1 = document.querySelector('#flaga1');
+    const flaga2 = document.querySelector('#flaga2');
+    flaga1.setAttribute('src', 'dobrze.jpeg');
+    flaga2.setAttribute('src', 'zle.png');
+    setTimeout(() => {
+        losowanie();
+    }, 1500);
         if (popr > rekord) {
             rekord = popr;
             localStorage.setItem('rekord', rekord);
         }
-        losowanie();
     }
 
     if (country2.population > country1.population) {
         niep++;
+        vid.src = 'videoplayback.mp4'
+        vid.autoplay = true
+        vid.loop = false
+        const flaga1 = document.querySelector('#flaga1');
+    const flaga2 = document.querySelector('#flaga2');
+    flaga1.setAttribute('src', 'zle.png');
+    flaga2.setAttribute('src', 'dobrze.jpeg');
+    setTimeout(() => {
         losowanie();
+    }, 1500);
+        
     }
 }
 
@@ -90,15 +112,33 @@ async function prawy() {
 
     if (country1.population > country2.population) {
         niep++;
+        vid.src = 'videoplayback.mp4'
+        vid.autoplay = true
+        vid.loop = false
+        const flaga1 = document.querySelector('#flaga1');
+    const flaga2 = document.querySelector('#flaga2');
+    flaga1.setAttribute('src', 'dobrze.jpeg');
+    flaga2.setAttribute('src', 'zle.png');
+    setTimeout(() => {
         losowanie();
+    }, 1500);
     }
 
     if (country2.population > country1.population) {
         popr++;
+        vid.src = 'dobrze.mp4'
+        vid.autoplay = true
+        vid.loop = false
+        const flaga1 = document.querySelector('#flaga1');
+    const flaga2 = document.querySelector('#flaga2');
+    flaga1.setAttribute('src', 'zle.png');
+    flaga2.setAttribute('src', 'dobrze.jpeg');
+    setTimeout(() => {
+        losowanie();
+    }, 1500);
         if (popr > rekord) {
             rekord = popr;
             localStorage.setItem('rekord', rekord);
         }
-        losowanie();
     }
 }
